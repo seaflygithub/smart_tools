e4b6b6f64d0f818e2a3b7c27f49abbdcf2288206
SeaflyDennis <seafly0616@qq.com>
2017-09-17 11:38:25 +0800

    fix: sudo for translate install

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ source ${CUR_DIR}/global_sys.sh
 
 chmod 755 ${CUR_DIR}/*.sh
 ${SUDO} cp ${CUR_DIR}/*.sh /usr/bin
-cd ${CUR_DIR} ./translate/ ; make install
+cd ${CUR_DIR}/translate/ ; make install
 
 rm -rf ${CUR_DIR}/install
